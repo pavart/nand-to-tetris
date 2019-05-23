ea83e4262717d223eddf216901f14fe86ce29c05
Artem Pavlenko <artem@pavart.dev>
2019-05-23 23:42:38 +0300

    Change bash to #sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 DIRECTORY=$1
 GOOD_RESULT="End of script - Comparison ended successfully"
 FAILED=0
@@ -6,7 +6,7 @@ FAILED=0
 for i in $(find . -name '*.tst' | sort); do
 
     SIMULATION_RESULT=$(sh tools/HardwareSimulator.sh $i;)
-    if [[ "$SIMULATION_RESULT" == "$GOOD_RESULT" ]];
+    if [ "$SIMULATION_RESULT" == "$GOOD_RESULT" ];
     then
         echo "$i - tests passed"
     else
@@ -16,7 +16,7 @@ for i in $(find . -name '*.tst' | sort); do
 
 done
 
-if [[ "$FAILED" == "1" ]];
+if [ "$FAILED" == "1" ];
 then
     echo "Tests failed!";
     exit 1;
