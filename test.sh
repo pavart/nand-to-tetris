c73fe856893b577876f006f04ecdbaed8d8c9437
Artem Pavlenko <artem@pavart.dev>
2019-05-23 23:53:27 +0300

    Travis integration (#4)
    Add Travis CI support

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+DIRECTORY=$1
+GOOD_RESULT="End of script - Comparison ended successfully"
+FAILED=0
+
+for i in $(find . -name '*.tst' | sort); do
+
+    SIMULATION_RESULT=$(sh tools/HardwareSimulator.sh $i;)
+    if [[ "$SIMULATION_RESULT" == "$GOOD_RESULT" ]];
+    then
+        echo "$i - tests passed"
+    else
+        echo "$i - tests failed"
+        FAILED=1
+   fi
+
+done
+
+if [[ "$FAILED" == "1" ]];
+then
+    echo "Tests failed!";
+    exit 1;
+else
+    echo "All tests passed!"
+fi
